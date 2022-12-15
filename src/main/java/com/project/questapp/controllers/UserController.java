afda5d759804fd932820c3f54dd1e4555b1270df
busra <busrabal.802@gmail.com>
2022-12-15 17:13:43 +0300

    15.12.2022

--- src/main/java/com/project/questapp/controllers/UserController.java
+++ src/main/java/com/project/questapp/controllers/UserController.java
@@ -39,7 +39,7 @@ public class UserController {
 	@GetMapping("/{userId}")
 	public User getOneUser(@PathVariable Long userId) {
 		// custom exception add
-		return userService.getOneUser(userId);
+		return userService.getOneUserById(userId);
 	}
 
 	@PutMapping("/{userId}")
